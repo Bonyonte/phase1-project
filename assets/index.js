eec8b2a787556e691e5bd9af53cd5a518114979f
Bonyonte <godfrey.magogo@student.moringaschool.comgit config --global user.name Bonyontegit branch -m maingit branch -M maingit remote add origin git@github.com:Bonyonte/ip_1.gitgit push -u origin maingit config --global user.email godfrey.magogo@student.moringaschool.com>
2022-06-22 21:49:36 +0300

    add the right div

--- assets/index.js
+++ assets/index.js
@@ -1,9 +1,20 @@
 
-const findBooks = document.querySelector("Books")
+let url = 'http://localhost:3000/Books'
 
 
 
-fetch('http://localhost:3000/Books')
+fetch(url)
 	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
+	.then(data => {
+		console.log(data)
+
+		let html = ""
+
+		data.map((value) => {
+			html += `
+
+
+			`
+		})
+	})
+	
