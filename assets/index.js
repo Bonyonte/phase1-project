a03d23dd78d24cd272abdb65592ea70c629aa6ee
Bonyonte <godfrey.magogo@student.moringaschool.comgit config --global user.name Bonyontegit branch -m maingit branch -M maingit remote add origin git@github.com:Bonyonte/ip_1.gitgit push -u origin maingit config --global user.email godfrey.magogo@student.moringaschool.com>
2022-06-23 00:34:30 +0300

    fetch price

--- assets/index.js
+++ assets/index.js
@@ -16,8 +16,8 @@ fetch(url)
                       <h4 class="title">Title: ${value.title}</h4>
                       <h4 id="author">Author: ${value.author}</h4>
                    <img class="card-img-top" src="${value.imgUrl}" alt="Card image" style="width:70%">
-            //     <div class="card-body">
-            //       <a href="#" class="btn btn-primary">Price: ${value.price}</a>
+                 <div class="card-body">
+                   <a href="#" class="btn btn-primary">Price: ${value.price}</a>
 			// 	  <h4>Review: ${value.review}</h4>
             //       <p class="card-text">${value.description}</p>
 			// 	  <h4>Source: ${value.source} </h4>
