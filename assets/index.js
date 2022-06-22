1792cfab620fbfc56236b86423650aee925439ac
Bonyonte <godfrey.magogo@student.moringaschool.comgit config --global user.name Bonyontegit branch -m maingit branch -M maingit remote add origin git@github.com:Bonyonte/ip_1.gitgit push -u origin maingit config --global user.email godfrey.magogo@student.moringaschool.com>
2022-06-23 00:47:04 +0300

    adding span

--- assets/index.js
+++ assets/index.js
@@ -13,14 +13,14 @@ fetch(url)
 			html += `
 			 <div class = "col.md-9">
 			 <div class="card">
-                      <h4 class="title">Title: ${value.title}</h4>
-                      <h4 id="author">Author: ${value.author}</h4>
-                   <img class="card-img-top" src="${value.imgUrl}" alt="Card image" style="width:70%">
+                      <h4 class="title"><span>Title</span>: ${value.title}</h4>
+                      <h4 id="author"><span>Author</span>: ${value.author}</h4>
+                   <img class="card-img-top" src="${value.imgUrl}" alt="Card image" style="width:90%">
                  <div class="card-body">
                    <a href="#" class="btn btn-primary">Price: ${value.price}</a>
-			 	  <h4>Review: ${value.review}</h4>
+			 	  <h4><span>Review</span>: ${value.review}</h4>
                    <p class="card-text">${value.description}</p>
-			 	  <h4>Source: ${value.source} </h4>
+			 	  <h4><span>Source</span>: ${value.source} </h4>
                 </div>
              </div>
 			 </div>
